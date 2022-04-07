f3ea9c7e66182e29e884b94135da0c54465cd16d
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-04-07 22:29:50 +0530

    misc: remove unecessary logs

--- chromium/src/index.js
+++ chromium/src/index.js
@@ -1,13 +1,10 @@
 window.addEventListener("load", () => {
-  console.log("loaded");
   const linkElement = document.querySelector(".fileuploadsubmission a");
 
   if (!linkElement || !linkElement.getAttribute("href")) {
     console.log("EnhancedMoodle: No link element found");
     return;
   }
-
-  console.log(linkElement.getAttribute("href"));
 
   const url = new URL(linkElement.getAttribute("href"));
   const hasForceDownload = url.searchParams.get("forcedownload");
